1a9dc69c5d2ff85c6a4f129850ed55cbf641a44d
Anics35 <sahaanirban278@gmail.com>
2025-04-08 01:23:27 +0530

    Ep-2>> Igniting the APP

--- App.js
+++ App.js
@@ -1,35 +1,25 @@
-/*
-<div id="parent">
-  <div id="child">
-    <h1>I am  h1 tag</h1>
-     <h2>I am  h2 tag/h2>
-  </div>
-</div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-*/
-
-const parent = React.createElement(
+// Create parent element with two child divs
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
     "div",
-    { id: "parent" },
-  [  React.createElement(
-        "div",
-        { id: "child" },
-        [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag")
-        ]
-    ),
-
-    React.createElement(
-        "div",
-        { id: "child" } ,
-        [
-            React.createElement("h1", {}, "I am h1 tag"),
-            React.createElement("h2", {}, "I am h2 tag") ,           
-        ]
-    )
+    { id: "child1", key: "child1" },
+    [
+      React.createElement("h1", { key: "h1-1" }, "I am h1 tag"),
+      React.createElement("h2", { key: "h2-1" }, "I am h2 tag")
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2", key: "child2" },
+    [
+      React.createElement("h1", { key: "h1-2" }, "I am h1 tag"),
+      React.createElement("h2", { key: "h2-2" }, "I am h2 tag")
+    ]
+  )
 ]);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- SETUP.md
+++ SETUP.md
@@ -10,3 +10,35 @@ npm install -D parcel
 ```
 ### .gitignore
 ![Photo](images/1.png)
+
+### Igniting the App
+```bash
+npx parcel index.h
+``` 
+npx means executing the package
+
+### Installing React
+```bash
+npm install react 
+```
+### Installing React DOM
+```bash
+npm install react-dom
+```     
+
+### Importing React and ReactDOM
+```js
+import React from "react";
+import ReactDOM from "react-dom/client";
+```
+### Build a production-optimized bundle of our app
+```bash
+npx parcel build index.html
+```
+### Browser-list
+```js
+"browsersList":
+  [
+    "last 2 versions"
+  ]
+```
