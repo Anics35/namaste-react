9d9e87eb5d459b6bbb9ca72445102c1366e7c63c
Anics35 <sahaanirban278@gmail.com>
2025-04-09 17:53:14 +0530

    React-Functional-Component

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// Create parent element with two child divs
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading"
-  },
-  "HEY !!!",
+//JSX- React element
+const heading = (
+  <h1 className="head" tabIndex="1">
+    Namaste React using JSX
+  </h1>
+);
+
+
+
+/* React Component
+1.Class based component - OLD
+2.Functional Component - NEW
+*/
+const Title= ()=>(
+  <h1 className="head"
+  tabIndex="5">
+    Namaste React 
+  </h1>
 )
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title/>
+    <h1 className="Heading"> React Functional Component </h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+//root.render(heading);
+
+//rendering a functional component
+root.render(<HeadingComponent />);
